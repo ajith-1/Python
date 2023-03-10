bf9b9b398ce98d6f3b35e4d9cf1bddb923332d60
Ajith Kumar <118709855+ajith-1@users.noreply.github.com>
2023-03-10 11:30:47 +0530

    Update Basics.py

--- Basics.py
+++ Basics.py
@@ -25,3 +25,14 @@ fac=1
 for i in range(1,a+1):
     fac=fac*i
 print(fac)
+#  prime
+a=int(input("start : "))
+b=int(input("End : "))
+
+for i in range(a,b):
+   if (i>1):
+    for j in range(2,i):
+        if(i%j==0):
+           break
+    else:
+        print(i,"prime")
