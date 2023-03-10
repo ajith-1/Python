93973f9b4429252485db33ffbddf033d6d74e70e
Ajith Kumar <118709855+ajith-1@users.noreply.github.com>
2023-03-10 11:29:59 +0530

    Update Basics.py

--- Basics.py
+++ Basics.py
@@ -1,1 +1,27 @@
+# reverse
+n = int(input("Enter number: "))  
+  
+reverse = 0  
+  
+while(n>0):  
+    digit = n%10  
+    reverse = reverse*10+digit  
+    n=n//10  
+print("The reverse of the number:",reverse)  
 
+# palindrom
+a=int(input("start : "))
+c=str(a)
+b=(c[::-1])
+if(c==b):
+   print("palindrom")
+else:
+    print("not palindrom")
+    
+#     fact
+a=int(input("start : "))
+
+fac=1
+for i in range(1,a+1):
+    fac=fac*i
+print(fac)
